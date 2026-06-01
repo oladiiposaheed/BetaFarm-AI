2d1d151e1a6c554a196cfe5c6e50a589d6eb6787
Saheed <oladiiposaheed@gmail.com>
2026-06-01 22:34:07 +0100

    Fix render.yaml paths and add service requirements 18

--- rag/main.py
+++ rag/main.py
@@ -88,7 +88,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
-    allow_credentails=True,
+    allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
 )

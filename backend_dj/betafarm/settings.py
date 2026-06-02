738bdd636e8fde2376e9b56f71dd7fee06d07cd0
Saheed <oladiiposaheed@gmail.com>
2026-06-02 15:47:39 +0100

    Add frontend to the templates 5

--- backend_dj/betafarm/settings.py
+++ backend_dj/betafarm/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = "betafarm.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, '../frontend')],  
+        "DIRS": [BASE_DIR / 'templates'],  
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -153,12 +153,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production static files
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../frontend'),
+    os.path.join(BASE_DIR, 'static'),
 ] # For development static files 
 
 # Default primary key field type
